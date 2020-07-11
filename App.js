f94d569b5acef82672c27046d960edf44d86f0d8
Bartek <bk.koluch@gmail.com>
2020-07-11 11:51:55 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+	state = {
+		comics: [],
+	};
+
+	componentDidMount() {
+		axios
+			.get('https://xkcd.com/info.0.json')
+			.then((res) => {
+				console.log(res.data);
+				const data = res.data;
+				this.setState({
+					comics: data,
+				});
+			})
+			.catch(() => {
+				console.log('There was some error fetching the data');
+			});
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.paragraph}>{this.state.comics.title}</Text>
+				<StatusBar style='auto' />
+			</View>
+		);
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#2c275f',
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: '500px',
+	},
+	paragraph: {
+		color: '#fff',
+	},
 });

e7068d7fb7f8191beed84663e675b764e91e6dcd
Bartek <bk.koluch@gmail.com>
2020-07-11 19:08:22 +0200

    Added some styling to all comics view

--- components/Comic/Comic.js
+++ components/Comic/Comic.js
@@ -4,7 +4,7 @@ import { StyleSheet, View, Text, Image } from 'react-native';
 export default function (props) {
 	return (
 		<View style={styles.container}>
-			<Text>{console.log(props.title)}</Text>
+			<Text style={styles.title}>{props.title}</Text>
 			<Image
 				style={styles.image}
 				source={props.image}
@@ -17,21 +17,28 @@ export default function (props) {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
+		flexDirection: 'row',
 		backgroundColor: '#000',
 		alignItems: 'center',
-		justifyContent: 'center',
-		height: '50vh',
+		justifyContent: 'space-between',
 		marginBottom: '50px',
+		borderColor: '#ccc',
+		borderWidth: 5,
+		borderBottomLeftRadius: 20,
+		borderBottomRightRadius: 20,
+		borderTopLeftRadius: 20,
+		borderTopRightRadius: 20,
 	},
 	image: {
 		height: '100%',
-		width: '100%',
+		width: '200px',
 	},
 	title: {
 		color: '#fff',
 		fontFamily: 'Trebuchet MS, sans-serif',
-		fontSize: '2rem',
+		fontSize: '30px',
 		letterSpacing: '-2px',
 		textTransform: 'uppercase',
+		marginLeft: '5px',
 	},
 });

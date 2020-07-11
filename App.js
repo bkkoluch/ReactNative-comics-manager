ecc059088ab8d9e5396be279ffbaac09f9c23d98
Bartek <bk.koluch@gmail.com>
2020-07-11 14:35:25 +0200

    Added fetching of last 8 comics

--- App.js
+++ App.js
@@ -1,32 +1,61 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 import axios from 'axios';
 
 export default class App extends Component {
 	state = {
 		comics: [],
+		previousComic: null,
+		recentComicNum: 0,
 	};
 
 	componentDidMount() {
+		let comicNumber = 1;
 		axios
 			.get('https://xkcd.com/info.0.json')
-			.then((res) => {
-				console.log(res.data);
-				const data = res.data;
-				this.setState({
-					comics: data,
-				});
+			.then((response) => {
+				this.setState((prevState) => ({
+					comics: [...prevState.comics, response.data],
+					recentComicNum: response.data.num,
+				}));
+
+				for (let counter = 0; counter < 7; counter++) {
+					axios
+						.get(
+							'https://xkcd.com/' +
+								(this.state.recentComicNum - comicNumber) +
+								'/info.0.json'
+						)
+						.then((secondResponse) => {
+							this.setState((prevState) => ({
+								comics: [
+									...prevState.comics,
+									secondResponse.data,
+								],
+							}));
+						})
+						.catch((error) => {
+							console.log(error);
+						});
+					comicNumber++;
+				}
 			})
-			.catch(() => {
-				console.log('There was some error fetching the data');
+			.catch((error) => {
+				console.log(error);
 			});
 	}
 
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text style={styles.paragraph}>{this.state.comics.title}</Text>
+				<Text style={styles.text}>Here's some comic</Text>
+				<Text style={styles.text}>{this.state.comics.title}</Text>
+				<Image
+					source={this.state.comics.img}
+					style={styles.image}
+					resizeMode='contain'
+				/>
 				<StatusBar style='auto' />
 			</View>
 		);
@@ -36,12 +65,15 @@ export default class App extends Component {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#2c275f',
+		backgroundColor: '#000',
 		alignItems: 'center',
 		justifyContent: 'center',
-		width: '500px',
 	},
-	paragraph: {
+	image: {
+		height: '300px',
+		width: '100%',
+	},
+	text: {
 		color: '#fff',
 	},
 });
